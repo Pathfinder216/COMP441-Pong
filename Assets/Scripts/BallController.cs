46b9f2144f599a8b2c747f40d74b284220ddbbe9
Ben Green <benno.green@gmail.com>
2017-11-10 11:45:10 -0500

    Added ball controller script

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    private float speed = 5f;
+    private Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(1f, 0, Random.Range(-1f, 1f)) * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += movement * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        string tag = other.tag;
+        if (tag == "wall") {
+            movement.z *= -1;
+        }
+        else if (tag == "paddle") {
+            movement.x *= -1;
+        }
+    }
+}
