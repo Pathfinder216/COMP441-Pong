efc52868b529e06c2c5b02092f8e71d74afb726b
wytriam <thomas.m.frick@gmail.com>
2017-11-10 11:31:59 -0500

    added player 1/player 2 scripts

---
+++ Assets/Scripts/PlayerOneInput.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneInput : MonoBehaviour {
+
+	public enum InputKey
+	{
+		W = KeyCode.W,
+		A = KeyCode.A,
+		Up = KeyCode.UpArrow,
+		Down = KeyCode.DownArrow
+	}
+
+
+	public InputKey up;
+	public InputKey down;
+	public static float playerSpeed = 9f;
+	private CharacterController characterCtrl;
+	// Use this for initialization
+	void Start () {
+		characterCtrl = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = Vector3.zero;
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			movement = new Vector3 (0, 0, playerSpeed * Time.deltaTime);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			movement += new Vector3 (0, 0, playerSpeed * Time.deltaTime);
+		}
+
+		characterCtrl.Move (movement);
+	}
+}
