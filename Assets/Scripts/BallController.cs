e92cce863e452b0dc4182be4ec22c4c2cc5bea4f
Ben Green <benno.green@gmail.com>
2017-11-10 14:28:33 -0500

    didn't finish adding sound

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
     private float speed = 5f;
     private Vector3 movement;
+    private AudioSource audioSource;
+    public AudioClip hit, miss;
 
 	// Use this for initialization
 	void Start () {
         movement = new Vector3(1f, 0, Random.Range(-1f, 1f)) * speed;
+
     }
 	
 	// Update is called once per frame
@@ -18,6 +21,9 @@ public class BallController : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         string tag = other.tag;
+        audioSource.clip = hit;
+        audioSource.Play();
+
         if (tag == "wall") {
             movement.z *= -1;
         }
